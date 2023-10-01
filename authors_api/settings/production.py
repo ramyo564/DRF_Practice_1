32370cd78b71d00bd7837c77ddb08ad3070ea7b9
ramyo564 <yohan032yohan@gmail.com>
2023-10-02 04:05:31 +0900

    chore: setup basic django project

---
+++ authors_api/settings/production.py
@@ -0,0 +1,8 @@
+from .base import *  # noqa
+from .base import env
+
+
+ADMINS = [("Alpha Omondi Ogilo", "api.imperfect@gmail.com")]
+
+# TODO add domain names of the production server
+CSRF_TRUSTED_ORIGINS = [""]
